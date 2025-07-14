2c036fdd707dd85f6989502d2e48b609b785d416
Talha A. B. <talha.azim@gmail.com>
2025-07-14 00:41:15 -0400

    Added direnv and nix flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Pylox devshell flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let 
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    in {
+     devShells.${system}.default = pkgs.mkShell {
+       buildInputs = with pkgs; [
+	 python313 
+       ];
+     }; 
+     
+  };
+}
