37c0151c8562ddde291154a4a81bd213e988036a
Talha A. B. <talha.azim@gmail.com>
2025-12-27 00:54:52 -0500

    Implemented parser up to, but not including, syntax errors

--- src/pylox/parser.py
+++ src/pylox/parser.py
@@ -60,7 +60,7 @@ class Parser:
     def term(self) -> Expr:
         expr: Expr = self.factor()
 
-        while self.match(TokenType.SLASH, TokenType.STAR):
+        while self.match(TokenType.MINUS, TokenType.PLUS):
             operator: Token = self.previous()
             right: Expr = self.factor()
             expr = Binary(expr, operator, right)
@@ -68,4 +68,37 @@ class Parser:
         return expr
 
     def factor(self) -> Expr:
-        raise NotImplemented
+        expr: Expr = self.unary()
+        
+        while self.match(TokenType.SLASH, TokenType.STAR):
+            operator: Token = self.previous()
+            right: Expr = self.unary()
+            expr = Binary(expr, operator, right)
+        
+        return expr
+    
+    def unary(self) -> Expr:
+        if self.match(TokenType.BANG, TokenType.MINUS):
+            operator: Token = self.previous()
+            right: Expr = self.unary()
+            return Unary(operator, right)
+        return self.primary()
+
+    def primary(self) -> Expr:
+        
+        if self.match(TokenType.FALSE):
+            return Literal(False)
+        
+        if self.match(TokenType.TRUE):
+            return Literal(True)
+
+        if self.match(TokenType.NIL):
+            return Literal(None)
+
+        if self.match(TokenType.NUMBER, TokenType.STRING):
+            return Literal(self.previous().literal)
+        
+        if self.match(TokenType.LEFT_PAREN):
+            expr: Expr = self.expression()
+            self.consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.")
+            return Grouping(expr)
