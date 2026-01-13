dab61096857fb2a33756ca22b685e465e4122527
talha.azim <talha.azim@gmail.com>
2026-01-13 16:35:02 +0000

    feat: nested environments.

--- src/pylox/environment.py
+++ src/pylox/environment.py
@@ -2,7 +2,8 @@ from token import Token
 import runtimeerror
 class Environment():
 
-    def __init__(self):
+    def __init__(self, enclosing: 'Environment' = None) -> None:
+        self.enclosing = enclosing
         self.values = {}
 
     def define(self, name: str, value: object) -> None:
@@ -11,12 +12,19 @@ class Environment():
     def get(self, name: Token) -> object:
         if name.lexeme in self.values:
             return values[name.lexeme]
+        
+        if enclosing:
+            return enclosing[name]
 
         raise runtimeerror.RuntimeError(name, "Undefined variable '" + name.lexeme + "'.")
     
     def assign(name: Token, value: object) -> None:
         if name.lexeme in self.values:
             self.values[name.lexeme] = value
+            return None
+        
+        if enclosing:
+            enclosing.assign(name, value)
             return None
         
         raise runtimeerror.RuntimeError(name, "Undefined variable '" + name.lexeme + "'.")
