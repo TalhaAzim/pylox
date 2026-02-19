ddcf40c2e5f036c83dc28e8a359b6a8a4528bf76
talha.azim <talha.azim@gmail.com>
2026-02-19 21:21:44 +0000

    C17.3 Set up bytecode emission prereqs - compiler.c:   - add currentChunk(), emitByte(), emitBytes() and emitReturn()   - wiring for compile() (wip)
    Continue at 17.4

--- src/clox/compiler.c
+++ src/clox/compiler.c
@@ -13,6 +13,9 @@ typedef struct {
 } Parser;
 
 Parser parser;
+Chunk *compilingChunk;
+
+static Chunk *currentChunk() { return compilingChunk; }
 
 static void errorAt(Token *token, const char *message) {
   if (parser.panicMode)
@@ -57,12 +60,25 @@ static void consume(TokenType type, const char *message) {
   }
 }
 
+static void emitByte(uint8_t byte) {
+  writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+  emitByte(byte1);
+  emitByte(byte2);
+}
+
+static void emitReturn() { emitByte(OP_RETURN); }
+
 bool compile(const char *source, Chunk *chunk) {
   initScanner(source);
+  compilingChunk = chunk;
   parser.hadError = false;
   parser.panicMode = false;
   advance();
   expression();
   consume(TOKEN_EOF, "Expect end of expression.");
+  endCompiler();
   return !parser.hadError;
 }
