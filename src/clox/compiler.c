3083bd52a6be5c9e485885d003e68f1cf9165c68
Talha A. B. <talha.azim@gmail.com>
2026-02-22 01:16:57 -0500

    C17.7 Added debug printing
    - common.h: added debug print flag;
    - compiler.c: added debut printing;
      - Added conditional debug.h include;
      - Added conditional printig to endCompiler().
    
    Yes, I messed up the chapter references in the last commit.
    
    Continue at Chapter 18.

--- src/clox/compiler.c
+++ src/clox/compiler.c
@@ -5,6 +5,10 @@
 #include "compiler.h"
 #include "scanner.h"
 
+#ifdef DEBUG_PRINT_CODE
+#include "debug.h"
+#endif
+
 typedef struct {
   Token current;
   Token previous;
@@ -93,7 +97,14 @@ static void emitBytes(uint8_t byte1, uint8_t byte2) {
 
 static void emitReturn() { emitByte(OP_RETURN); }
 
-static void endCompiler() { emitReturn(); }
+static void endCompiler() {
+  emitReturn();
+#ifdef DEBUG_PRINT_CODE
+  if (!parser.hadError) {
+    disassembeChunk(currentChunk(), "code");
+  }
+#endif
+}
 
 static void expression();
 static ParseRule *getRule(TokenType type);
