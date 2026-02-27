e9b10631ef017f90d1ae68ffee666c136511a933
talha.azim <talha.azim@gmail.com>
2026-02-27 18:32:03 +0000

    C18.4 Implemented nil and booleans.  - chunk.h: added opcodes for nil, booleans, and comparisons.  - compiler.c: added parsing for the above.  - debug.c: updated disassembleInstruction() for new opcodes.  - value.c:    - updated printValue() for new types.    - added valuesEqual() for comparisons.  - value.h: added prototype for valuesEqual().  - vm.c: updated run() for new opcodes.

--- src/clox/vm.c
+++ src/clox/vm.c
@@ -40,6 +40,10 @@ Value pop() {
 
 static Value peek(int distance) { return vm.stackTop[-1 - distance]; }
 
+static bool isFalsey(Value value) {
+  return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
@@ -72,6 +76,27 @@ static InterpretResult run() {
       push(constant);
       break;
     }
+    case OP_NIL:
+      push(NIL_VAL);
+      break;
+    case OP_TRUE:
+      push(BOOL_VAL(true));
+      break;
+    case OP_FALSE:
+      BOOL_VAL(false);
+      break;
+    case OP_EQUAL: {
+      Value b = pop();
+      Value a = pop();
+      push(BOOL_VAL(valuesEqual(a, b)));
+      break;
+    }
+    case OP_GREATER:
+      BINARY_OP(BOOL_VAL, >);
+      break;
+    case OP_LESS:
+      BINARY_OP(BOOL_VAL, <);
+      break;
     case OP_ADD:
       BINARY_OP(NUMBER_VAL, +);
       break;
@@ -83,6 +108,9 @@ static InterpretResult run() {
       break;
     case OP_DIVIDE:
       BINARY_OP(NUMBER_VAL, /);
+      break;
+    case OP_NOT:
+      push(BOOL_VAL(isFalsey(pop())));
       break;
     case OP_NEGATE: {
       if (!IS_NUMBER(peek(0))) {
